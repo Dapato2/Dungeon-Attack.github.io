a7598b910b3904cc5aa7d53cbbef024dfb425178
Dapato2 <108306801+Dapato2@users.noreply.github.com>
2022-11-12 08:20:41 -0600

    Juego Terminado

--- js/defaultVars.js
+++ js/defaultVars.js
@@ -1,5 +1,6 @@
 const canvas = document.getElementById("my-canvas");
 const ctx = canvas.getContext("2d");
+ctx.font = "100px serif";
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
@@ -13,3 +14,10 @@ let frames = 0;
 let requestId = undefined
 
 const arrEnemies = []
+
+let points = 0 ;
+
+const audio = new Audio() // *
+audio.src = "./mp3/Komiku_-_70_-_Ending.mp3";
+audio.volume = 0.1; 
+audio.loop = true
--- js/script.js
+++ js/script.js
@@ -4,12 +4,67 @@ window.onload = function () {
     const hero = new Hero(heroX, heroY, 64, 64, true)
     //let enemy = new Enemy(canvas.width / 2, 20, 64, 64)
 
+    const btn = document.getElementById("start-game");
+    const menu = document.getElementById("menu-container");
+    const credits = document.getElementById("credits")
+    const instruCont = document.getElementById("instru-container");
+    const btnInstructions = document.getElementById("insructions");
+    const back1 = document.getElementById("back1");
+    const logo = document.getElementById("logo");
+    const imgInstru = document.getElementById("imgInstru")
+    const playAgain = document.getElementById("playAgain");
+    btn.addEventListener("click", function () {
+        startGame()
+        menu.style.display = "none" 
 
-    startGame();
+        setInterval(() => {
+            let x = Math.floor(Math.random() * canvas.width);
+            let y = 80;
+            let angle = Math.atan((hero.heroY - y) / (hero.heroX - x));
+            let velocity;
+            if ((hero.heroX - x) < 0) {
+                velocity = {
+                    x: -1 * Math.cos(angle),
+                    y: -1 * Math.sin(angle)
+                }
+            } else {
+                velocity = {
+                    x: Math.cos(angle),
+                    y: Math.sin(angle)
+                }
+            }
+            let enemy = new Enemy(x, y, 64, 64, velocity)
+            arrHorda.push(enemy)
+        }, 200);
+    })
+    btnInstructions.addEventListener("click", function () {
+    btn.style.display = "none"
+    btnInstructions.style.display = "none"
+    instruCont.style.display = "initial"
+    logo.style.display = "none"
+    imgInstru.style.display = "initial"
+    back1.style.display = "inicial"
+
+
+    })
+    back1.addEventListener("click", function () {
+        btn.style.display = "initial"
+        btnInstructions.style.display = "initial"
+        instruCont.style.display = "initial"
+        logo.style.display = "initial"
+        imgInstru.style.display = "none"
+        back1.style.display = "none"
+
+        })
+        
+
+
     function startGame() {
-        requestId = requestAnimationFrame(updateGame)
 
-
+        if(!requestId){
+            audio.play()
+            requestId = requestAnimationFrame( updateGame )
+          }
     };
 
     const arrProjectiles = [];
@@ -32,40 +87,21 @@ window.onload = function () {
         arrProjectiles.push(projectile)
     })
 
-    function collision (projectile,enemy){
-        if( (projectile.x + projectile.radius) > enemy.x && (projectile.x - projectile.radius) < (enemy.x + enemy.width) ){
-            if((projectile.y + projectile.radious) > enemy.y && (projectile.y - projectile.radious) < (enemy.y + enemy.height))
-            {
+    function collision(projectile, enemy) {
+        if ((projectile.x + projectile.radius) > enemy.x && (projectile.x - projectile.radius) < (enemy.x + enemy.width)) {
+            if ((projectile.y + projectile.radious) > enemy.y && (projectile.y - projectile.radious) < (enemy.y + enemy.height)) {
                 return true;
-             
-            }else{
+
+            } else {
                 return false;
             }
         }
-        else{
+        else {
             return false;
         }
     }
 
-    setInterval(() => {
-        let x = Math.floor(Math.random() * canvas.width);
-        let y = 80;
-        let angle = Math.atan((hero.heroY - y) / (hero.heroX - x));
-        let velocity;
-        if ((hero.heroX - x) < 0) {
-            velocity = {
-                x: -1 * Math.cos(angle),
-                y: -1 * Math.sin(angle)
-            }
-        } else {
-            velocity = {
-                x: Math.cos(angle),
-                y: Math.sin(angle)
-            }
-        }
-        let enemy = new Enemy(x, y, 64, 64, velocity)
-        arrHorda.push(enemy)
-    }, 200);
+
 
     function updateGame() {
         frames++;
@@ -74,25 +110,33 @@ window.onload = function () {
         bg.update();
         hero.update();
         //enemy.update();
-
+        ctx.font = "50px Fantasy";
+        ctx.fillText(`Points: ${points}`,canvas.width /2 - 200,100)
 
         arrProjectiles.forEach((element) => {
             element.update();
         })
 
-        arrHorda.forEach((element,index) => {
+        arrHorda.forEach((element, index) => {
             element.update();
-            arrProjectiles.forEach((element2,index2) => {
-                
-                if( (element2.x + element2.radius) > element.x && (element2.x - element2.radius) < (element.x + element.width) ){
-                    if((element2.y + element2.radius) > element.y && (element2.y - element2.radius) < (element.y + element.height)){
-                        arrHorda.splice(index,1);
-                        arrProjectiles.splice(index2,1)
+            arrProjectiles.forEach((element2, index2) => {
+
+                if ((element2.x + element2.radius) > element.x && (element2.x - element2.radius) < (element.x + element.width)) {
+                    if ((element2.y + element2.radius) > element.y && (element2.y - element2.radius) < (element.y + element.height)) {
+                        arrHorda.splice(index, 1);
+                        arrProjectiles.splice(index2, 1)
+                        points += 5
                     }
                 }
+                
             })
+            
+            if(hero.colision(element)){
+                gameOver();
+                playAgain.style.display = "initial";
+            }
         })
-        
+
 
         if (requestId) {
             requestId = requestAnimationFrame(updateGame)
